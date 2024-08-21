f18ada1b0fe637120073ba3dcb9cb7970ad2d0d2
Chieko Sato <chiekosato.02@gmail.com>
2024-08-21 20:30:06 +0900

    fix: class->className in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer class="footer fixed-bottom mt-auto py-3 bg-body-tertiary">
-      <div class="container">
-        <span class="text-body-secondary">&copy; 2024 Altera assignment</span>
+    <footer className="footer fixed-bottom mt-auto py-3 bg-body-tertiary">
+      <div className="container">
+        <span className="text-body-secondary">&copy; 2024 Altera assignment</span>
       </div>
     </footer>
   );
