1ab751a4f9de146f5066a1806ee3f19f7485920c
Chieko Sato <chiekosato.02@gmail.com>
2024-08-24 15:56:28 +0900

    feat: fetch products from backend

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,10 +1,41 @@
 import Product from "./Product";
-//import products from './products.json';
-import { useState } from "react";
-import productsAsStaticJson from "./products.json";
+import { useState, useEffect } from "react";
 
 const Catalog = () => {
-  const [products, setProducts] = useState(productsAsStaticJson);
+  const [products, setProducts] = useState(null);
+  const productsUrl = "data/products.json";
+  const [isLoading, setIsLoading] = useState(true);  
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch(productsUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(jsonData => {
+        setProducts(jsonData);
+      })
+      .catch(err => {
+        setError(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <>
       <div id="catalog">
